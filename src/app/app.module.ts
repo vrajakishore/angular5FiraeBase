16d35a7b8d8af543cdc4d4b67158c23cc83a980a
vrajakishore <vrajakishore2@gmail.com>
2017-12-14 18:22:12 +0530

    firebase integrated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,18 @@ import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DataService } from './data.service';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDJdilflWhwTXcx2u_lZ_l9w6ilC7BvBVs",
+  authDomain: "myrooster-70111.firebaseapp.com",
+  databaseURL: "https://myrooster-70111.firebaseio.com",
+  projectId: "myrooster-70111",
+  storageBucket: "",
+  messagingSenderId: "188965762142"
+};
 
 @NgModule({
   declarations: [
@@ -22,7 +34,9 @@ import { DataService } from './data.service';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(firebaseConfig),  // Add this
+    AngularFirestoreModule                            // And this
   ],
   providers: [DataService],
   bootstrap: [AppComponent]
