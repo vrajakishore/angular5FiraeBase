678f364b74fb9abe1a92f2fb6837a8fbcad47f4d
vrajakishore <vrajakishore2@gmail.com>
2017-12-16 16:12:40 +0530

    content page added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';     // Add this
 import { AboutComponent } from './about/about.component'; 
+import { ContentComponent } from './content/content.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'content',
+    component: ContentComponent
   },
   {
     path: 'about/:id',
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { ContentComponent } from './content/content.component';
 
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -28,6 +29,7 @@ var firebaseConfig = {
   declarations: [
     AppComponent,
     HomeComponent,
+    ContentComponent,
     AboutComponent
   ],
   imports: [
